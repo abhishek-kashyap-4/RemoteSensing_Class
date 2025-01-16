8cc6c783a04383d0419ecf8325b2d949c8167419
abhishek <kashyapabhishek19898@gmail.com>
2025-01-16 13:52:58 +0530

    Added Stratified Vector Data

---
+++ untitled0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 19:19:21 2025
+
+@author: kashy
+"""
+
+import fiona
+import geopandas as gpd
+
+gpkg = fiona.open("others/landcover2021-vector-gpkg/landcover2021-vector_gpkg/landcover2021-vector-Geopackage.gpkg")
